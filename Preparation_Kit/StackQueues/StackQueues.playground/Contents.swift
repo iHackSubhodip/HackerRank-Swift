61d1e0e53e05d3e3874ab5d26abf042a1bd3348e
Banerjee, Subhodip <differentialthinking@gmail.com>
2018-10-05 15:05:21 +0530

    is Balanced commit.

--- Preparation_Kit/StackQueues/StackQueues.playground/Contents.swift
+++ Preparation_Kit/StackQueues/StackQueues.playground/Contents.swift
@@ -7,3 +7,33 @@ import Foundation
  * Question Link: https://www.hackerrank.com/challenges/balanced-brackets/ [Balanced Brackets]
  *
  */
+
+
+func isBalanced(s: String) -> String {
+    var characterStack = [Character]()
+    
+    for aChar in s{
+        if aChar == "(" || aChar == "{" || aChar == "["{
+            characterStack.append(aChar)
+        }else if aChar == ")"{
+            guard characterStack.count > 0 && characterStack.removeLast() == "(" else{
+                return "NO"
+            }
+        }else if aChar == "}"{
+            guard characterStack.count > 0 && characterStack.removeLast() == "{" else{
+                return "NO"
+            }
+        }else if aChar == "]"{
+            guard characterStack.count > 0 && characterStack.removeLast() == "[" else{
+                return "NO"
+            }
+        }
+    }
+    
+    return characterStack.isEmpty ? "YES" : "NO"
+}
+
+isBalanced(s: "{{[[(())]]}}")
+
+
+
