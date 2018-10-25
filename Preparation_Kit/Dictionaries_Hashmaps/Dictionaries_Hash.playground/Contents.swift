7254acdfcd9e9d06ad98da2713edf54cc381ecfd
Banerjee, Subhodip <differentialthinking@gmail.com>
2018-10-25 18:17:40 +0530

    Hash Tables: Ransom Note solved.

--- Preparation_Kit/Dictionaries_Hashmaps/Dictionaries_Hash.playground/Contents.swift
+++ Preparation_Kit/Dictionaries_Hashmaps/Dictionaries_Hash.playground/Contents.swift
@@ -13,3 +13,35 @@ import UIKit
  * Output:
  *  Yes
  */
+
+func checkMagazine(magazine: [String], note: [String]) -> Void {
+    var magazineDictionary = [String: Int]()
+    
+    for aWord in magazine{
+        if let _ = magazineDictionary[aWord]{
+            magazineDictionary[aWord]! += 1
+        }else{
+            magazineDictionary[aWord] = 1
+        }
+    }
+    
+    for aWord in note{
+        if let amountOfWord = magazineDictionary[aWord]{
+            if amountOfWord < 1{
+                print("No")
+                return
+            }else{
+                magazineDictionary[aWord]! -= 1
+            }
+        }else{
+            print("No")
+            return
+        }
+    }
+    //print(magazineDictionary)
+    print("Yes")
+}
+
+checkMagazine(magazine: "give me one grand today night".components(separatedBy: " "), note: "give one grand today".components(separatedBy: " "))
+
+
