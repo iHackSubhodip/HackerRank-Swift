d40be4657864a5dc432f171151593be509140915
Banerjee, Subhodip <differentialthinking@gmail.com>
2018-10-25 15:37:20 +0530

    Dictionary Commit.

---
+++ Preparation_Kit/Dictionaries_Hashmaps/Dictionaries_Hash.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/**
+ * Question Link: https://www.hackerrank.com/challenges/ctci-ransom-note/ [Hash Tables: Ransom Note]
+ *
+ * Input:
+ *  6 4
+ *  give me one grand today night
+ *  give one grand today
+ *
+ * Output:
+ *  Yes
+ */
---
+++ Preparation_Kit/Dictionaries_Hashmaps/README.md
@@ -0,0 +1,3 @@
+# HackerRank-Swift
+
+### Please update the solved problems Link.
