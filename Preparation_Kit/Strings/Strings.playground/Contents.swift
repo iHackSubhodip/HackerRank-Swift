6524ce42ba6e34ad463009a83062be16f3d2d732
Banerjee, Subhodip <differentialthinking@gmail.com>
2018-10-31 16:44:52 +0530

    Anagrams commit.

--- Preparation_Kit/Strings/Strings.playground/Contents.swift
+++ Preparation_Kit/Strings/Strings.playground/Contents.swift
@@ -2,4 +2,38 @@
 
 import UIKit
 
-var str = "Hello, playground"
+/**
+ * Question Link: https://www.hackerrank.com/challenges/ctci-making-anagrams/ [Strings: Making Anagrams]
+ */
+
+func makeAnagram(a: String, b: String) -> Int {
+    var count = 0
+    
+    var firstArray = Array(a)
+    var secondArray = Array(b)
+    
+    for i in firstArray{
+        if secondArray.contains(i){
+            firstArray.remove(at: firstArray.index(of: i)!)
+            secondArray.remove(at: secondArray.index(of: i)!)
+        }
+    }
+    
+    count += firstArray.count + secondArray.count
+    
+    return count
+}
+
+makeAnagram(a: "cde", b: "abc")
+
+
+
+/**
+ * Question Link: https://www.hackerrank.com/challenges/alternating-characters/ [Alternating Characters]
+ */
+
+
+
+
+
+
